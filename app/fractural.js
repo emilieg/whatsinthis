2d96b1bed3f73bb1134b9a26ab7e4bece089b73b
keidra <keidrabaldwin@gmail.com>
2016-04-16 16:48:13 -0700

    added code for testing pulling information from factual API

--- app/fractural.js
+++ app/fractural.js
@@ -3,24 +3,33 @@ var request = require('request');
 var cheerio = require('cheerio');
 var Promise = require('bluebird');
 var Firebase = require('../models/Firebase');
+var Factual = require('factual-api');
+var factual = new Factual('ucdlw2DnmCkN0BkFUNWu8tnwGYG2xsm6ySqIqAp1', 'BGYXhK0r6FpO1XXWsOgJP5qE15GjjQVJNhzgyw0x');
 
 // use this as a way to test your api calls
 // node test/testWhatsIsThis.js will call this method
 exports.go = function() {
     // build your api calls here
 
-    //var key = "whatevery key"
+    // var key = "ucdlw2DnmCkN0BkFUNWu8tnwGYG2xsm6ySqIqAp1"
     //var url = "http://toxgate.nlm.nih.gov/cgi-bin/sis/search2/x?dbs+<db>:<keyword>?key=" + key;
     //
-    var url = "http://www.google.com"
-    console.log("testing api calls");
+    // var url = "http://api.v3.factual.com/t/products-cpg?q=shampoo?key=";
+    // console.log("testing api calls");
 
-    return new Promise(function (resolve, reject) {
-        request(url, function (error, response, html){
-            if (error) {
-                reject(error);
-            }
-            console.log(response);
-        })
-    })
+    // return new Promise(function (resolve, reject) {
+    //     request(url, function (error, response, body){
+    //         if (error) {
+    //             reject(error);
+    //         }
+    //         console.log(response);
+    //         return JSON.parse(body);
+    //     })
+    // })
+
+    factual.get('/t/products-cpg-nutrition?q=yoplait', function (error, res) {
+        console.log(res.data[0].ingredients);
+});
+
+
 }
