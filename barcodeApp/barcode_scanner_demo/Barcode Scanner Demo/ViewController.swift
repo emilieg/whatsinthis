c3620dac1153c9cef4acaef26b89070c4aa740ee
lawmicha <mlaw135@gmail.com>
2016-04-16 22:33:49 -0700

    xcode project

---
+++ barcodeApp/barcode_scanner_demo/Barcode Scanner Demo/ViewController.swift
@@ -0,0 +1,108 @@
+import UIKit
+import AVFoundation
+
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    let session         : AVCaptureSession = AVCaptureSession()
+    var previewLayer    : AVCaptureVideoPreviewLayer!
+    var highlightView   : UIView = UIView()
+    
+    var firstTime:Bool = true
+    
+    override func viewWillAppear(animated: Bool) {
+        print("welcome back")
+        if firstTime == true
+        {
+            firstTime = false
+            return
+        }
+        
+        // Start the scanner. You'll have to end it yourself later.
+        session.startRunning()
+        // else we load the camera again
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ViewController")
+        // Allow the view to resize freely
+        //self.highlightView.autoresizingMask =   [UIViewAutoresizing.FlexibleTopMargin, UIViewAutoresizing.FlexibleBottomMargin, UIViewAutoresizing.FlexibleLeftMargin, UIViewAutoresizing.FlexibleRightMargin]
+
+        // Select the color you want for the completed scan reticle
+        //self.highlightView.layer.borderColor = UIColor.greenColor().CGColor
+        //self.highlightView.layer.borderWidth = 3
+    
+        // Add it to our controller's view as a subview.
+        //self.view.addSubview(self.highlightView)
+
+        // For the sake of discussion this is the camera
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: device) as AVCaptureDeviceInput
+            session.addInput(input)
+        }catch let error as NSError {
+            print("nooo")
+            print(error)
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        session.addOutput(output)
+        output.metadataObjectTypes = output.availableMetadataObjectTypes
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        //previewLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as AVCaptureVideoPreviewLayer
+        previewLayer.frame = self.view.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.addSublayer(previewLayer)
+
+        // Start the scanner. You'll have to end it yourself later.
+        session.startRunning()
+    }
+
+    // This is called when we find a known barcode type with the camera.
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+
+        //var highlightViewRect = CGRectZero
+        //var barCodeObject : AVMetadataObject!
+
+        var detectionString : String!
+
+        let barCodeTypes = [AVMetadataObjectTypeUPCECode,
+            AVMetadataObjectTypeCode39Code,
+            AVMetadataObjectTypeCode39Mod43Code,
+            AVMetadataObjectTypeEAN13Code,
+            AVMetadataObjectTypeEAN8Code,
+            AVMetadataObjectTypeCode93Code,
+            AVMetadataObjectTypeCode128Code,
+            AVMetadataObjectTypePDF417Code,
+            AVMetadataObjectTypeQRCode,
+            AVMetadataObjectTypeAztecCode
+        ]
+
+        // The scanner is capable of capturing multiple 2-dimensional barcodes in one scan.
+        for metadata in metadataObjects {
+            for barcodeType in barCodeTypes {
+                if metadata.type == barcodeType {
+                    //barCodeObject = self.previewLayer.transformedMetadataObjectForMetadataObject(metadata as! AVMetadataMachineReadableCodeObject)
+                    //highlightViewRect = barCodeObject.bounds
+                    detectionString = (metadata as! AVMetadataMachineReadableCodeObject).stringValue
+                    print(detectionString)
+                    self.session.stopRunning()
+                    
+                    let whatsInThisViewController = self.storyboard?.instantiateViewControllerWithIdentifier("WhatsInThisViewControllerIdentifier") as? WhatsInThisViewController
+                    whatsInThisViewController?.productCode = detectionString
+                    self.navigationController?.pushViewController(whatsInThisViewController!, animated: true)
+                    
+                    break
+                }
+            }
+        }
+
+        //self.highlightView.frame = highlightViewRect
+        //self.view.bringSubviewToFront(self.highlightView)
+    }
+}
+
---
+++ barcodeApp/barcode_scanner_demo/Barcode Scanner Demo/WhatsInThisViewController.swift
@@ -0,0 +1,48 @@
+//
+//  WhatsInThisViewController.swift
+//  Barcode Scanner Demo
+//
+//  Created by Michael Law on 2016-04-16.
+//  Copyright © 2016 Bowst. All rights reserved.
+//
+
+import UIKit
+
+class WhatsInThisViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var productCodeLabel: UILabel!
+    
+    var productCode:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Loading WhatsInThisViewController with " + productCode)
+
+        productCodeLabel.text = productCode
+        let urlString = "https://ancient-springs-86295.herokuapp.com/" + productCode
+        print("my url string" + urlString)
+        let url = NSURL (string: urlString)
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
