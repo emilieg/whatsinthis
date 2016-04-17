396595a0528631dd3f924f55a3820b96fc7248d8
keidra <keidrabaldwin@gmail.com>
2016-04-17 10:24:21 -0700

    changed some ingredient definitions

--- models/food-data.js
+++ models/food-data.js
@@ -1,59 +1,58 @@
 exports.data = {
-    "carbonated water": {
+    "Carbonated water": {
       def: "Water that has been pressurized with carbon dioxide",
       moreinfo: null
     },
-    "sucrose": {
+    "Sucrose": {
       def: "Sugar",
       moreinfo: null
     },
-    "glucose": {
+    "Glucose": {
       def: "Sugar obtained from plants or fruits",
       moreinfo: null
     },
-    "citric acid": {
+    "Sodium citrate": {
       def: "Weak acid found in many citrus fruits",
       moreinfo: null
     },
-    "taurine" : {
+    "Taurine" : {
         def: "A colorless crystalline substance obtained from the bile of mammals",
         moreinfo: null
     },
-      "sodium bicarbonate" : {
-        def: "Baking Soda",
-        moreinfo: null
-    },
-      "magnesium bicarbonate" : {
-        def: "Bicarbonate salt of magnesium",
-        moreinfo: null
-    },
-      "caffeine" : {
+      "Caffeine" : {
         def: "A natural stimulant derived from plants",
         moreinfo: null
     },
-      "niacinamide" : {
+      "Niacin" : {
         def: "Vitamin B",
         moreinfo: null
     },
-       "calcium pantothenate" : {
-        def: "A calcium salt derived from vitamin B",
-        moreinfo: null
-    },
-       "pyridoxine hcl" : {
+       "Pyridoxine HCL" : {
         def: "Vitamin B6",
         moreinfo: null
     },
-        "vitamin b12" : {
+        "Vitamin b12" : {
         def: "A vitamin that helps form red blood cells for oxygen formation",
         moreinfo: null
     },
-        "Natural and Artificial Flavors" : {
+        "Artificial Flavors" : {
         def: "Natural substances derived from animals or plants, and artificially created flavors",
         moreinfo: null
     },
-        "colors" : {
+        "Colors" : {
         def: "Dyes or pigments added to provide color",
         moreinfo: null
+    },
+       "D-Pantothenol" : {
+        def: "Definition",
+        moreinfo: null
+    },
+      "Inositol" : {
+        def: "Definition",
+        moreinfo: null
+    },
+      "Glucuronolactone" : {
+        def: "Definition",
+        moreinfo: null
     }
-
   }
