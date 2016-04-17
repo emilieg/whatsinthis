ac1980aa9c07f0615dd25b1771849be97af31977
montehoover <monte.b.hoover@gmail.com>
2016-04-16 19:04:44 -0700

    Added models/food-data.js for ingredients info.

---
+++ models/food-data.js
@@ -0,0 +1,10 @@
+exports.data = [
+    {name: "carbonated water",
+      def: "Safe water to drink"},
+    {name: "sucrose",
+      def: "Table sugar"},
+    {name: "glucose",
+      def: "Some gross sugar"},
+    {name: "citric acid",
+      def: "Mild acid derived from citris fruit, used as preservative"} 
+  ]
