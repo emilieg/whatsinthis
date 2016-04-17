b509b61042a9bbc993cde77e49d6b8b1797e9055
keidra <keidrabaldwin@gmail.com>
2016-04-17 09:50:16 -0700

    changed some ingredient definitions

--- models/food-data.js
+++ models/food-data.js
@@ -1,22 +1,59 @@
 exports.data = {
     "carbonated water": {
-      def: "Safe water to drink",
+      def: "Water that has been pressurized with carbon dioxide",
       moreinfo: null
     },
     "sucrose": {
-      def: "Table sugar",
+      def: "Sugar",
       moreinfo: null
     },
     "glucose": {
-      def: "Some gross sugar",
+      def: "Sugar obtained from plants or fruits",
       moreinfo: null
     },
     "citric acid": {
-      def: "Mild acid derived from citris fruit, used as preservative",
+      def: "Weak acid found in many citrus fruits",
       moreinfo: null
     },
-    "Alcohol" : {
-        def: " lol",
+    "taurine" : {
+        def: "A colorless crystalline substance obtained from the bile of mammals",
+        moreinfo: null
+    },
+      "sodium bicarbonate" : {
+        def: "Baking Soda",
+        moreinfo: null
+    },
+      "magnesium bicarbonate" : {
+        def: "Bicarbonate salt of magnesium",
+        moreinfo: null
+    },
+      "caffeine" : {
+        def: "A natural stimulant derived from plants",
+        moreinfo: null
+    },
+      "niacinamide" : {
+        def: "Vitamin B",
+        moreinfo: null
+    },
+       "calcium pantothenate" : {
+        def: "A calcium salt derived from vitamin B",
+        moreinfo: null
+    },
+       "pyridoxine hcl" : {
+        def: "Vitamin B6",
+        moreinfo: null
+    },
+        "vitamin b12" : {
+        def: "A vitamin that helps form red blood cells for oxygen formation",
+        moreinfo: null
+    },
+        "Natural and Artificial Flavors" : {
+        def: "Natural substances derived from animals or plants, and artificially created flavors",
+        moreinfo: null
+    },
+        "colors" : {
+        def: "Dyes or pigments added to provide color",
         moreinfo: null
     }
+
   }
