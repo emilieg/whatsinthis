2fd839d8ac864e4758ad473d898a820e44660b0a
montehoover <monte.b.hoover@gmail.com>
2016-04-17 11:35:50 -0700

    Added data to food-data.js.

--- models/food-data.js
+++ models/food-data.js
@@ -1,58 +1,127 @@
 exports.data = {
-    "Carbonated water": {
-      def: "Water that has been pressurized with carbon dioxide",
+  // Red Bull
+  "Carbonated water": {
+    def: "Water that has been pressurized with carbon dioxide",
+    moreinfo: null
+  },
+  "Sucrose": {
+    def: "Sugar",
+    moreinfo: null
+  },
+  "Glucose": {
+    def: "Sugar obtained from plants or fruits",
+    moreinfo: null
+  },
+  "Sodium citrate": {
+    def: "Weak acid found in many citrus fruits",
+    moreinfo: null
+  },
+  "Taurine" : {
+      def: "A colorless crystalline substance obtained from the bile of mammals",
+      moreinfo: null
+  },
+    "Caffeine" : {
+      def: "A natural stimulant derived from plants",
+      moreinfo: null
+  },
+    "Niacin" : {
+      def: "Vitamin B",
+      moreinfo: null
+  },
+     "Pyridoxine HCL" : {
+      def: "Vitamin B6",
+      moreinfo: null
+  },
+      "Vitamin b12" : {
+      def: "A vitamin that helps form red blood cells for oxygen formation",
+      moreinfo: null
+  },
+      "Artificial Flavors" : {
+      def: "Natural substances derived from animals or plants, and artificially created flavors",
+      moreinfo: null
+  },
+      "Colors" : {
+      def: "Dyes or pigments added to provide color",
+      moreinfo: null
+  },
+     "D-Pantothenol" : {
+      def: "Definition",
+      moreinfo: null
+  },
+    "Inositol" : {
+      def: "Definition",
+      moreinfo: null
+  },
+    "Glucuronolactone" : {
+      def: "Definition",
+      moreinfo: null
+  },
+// Dove Deoderant
+  "Aluminum Zirconium Tetrachlorohydrex Gly": {
+      def: "Mixture of aluminum and zirconium used as an antiperspirant for it's ability to obstruct pores in the skin and prevent sweat from leaving the body",
+      moreinfo: null
+  },
+  "Cyclopentasiloxane": {
+      def: "Organosilicon compound used in cosmetics as a lubricant and emollient",
       moreinfo: null
     },
-    "Sucrose": {
-      def: "Sugar",
-      moreinfo: null
-    },
-    "Glucose": {
-      def: "Sugar obtained from plants or fruits",
-      moreinfo: null
-    },
-    "Sodium citrate": {
-      def: "Weak acid found in many citrus fruits",
-      moreinfo: null
-    },
-    "Taurine" : {
-        def: "A colorless crystalline substance obtained from the bile of mammals",
-        moreinfo: null
-    },
-      "Caffeine" : {
-        def: "A natural stimulant derived from plants",
-        moreinfo: null
-    },
-      "Niacin" : {
-        def: "Vitamin B",
-        moreinfo: null
-    },
-       "Pyridoxine HCL" : {
-        def: "Vitamin B6",
-        moreinfo: null
-    },
-        "Vitamin b12" : {
-        def: "A vitamin that helps form red blood cells for oxygen formation",
-        moreinfo: null
-    },
-        "Artificial Flavors" : {
-        def: "Natural substances derived from animals or plants, and artificially created flavors",
-        moreinfo: null
-    },
-        "Colors" : {
-        def: "Dyes or pigments added to provide color",
-        moreinfo: null
-    },
-       "D-Pantothenol" : {
-        def: "Definition",
-        moreinfo: null
-    },
-      "Inositol" : {
-        def: "Definition",
-        moreinfo: null
-    },
-      "Glucuronolactone" : {
-        def: "Definition",
-        moreinfo: null
-    }
-  }
+  "Stearyl Alcohol": {
+    def: "Organic compound in form of white granules; used as an emollient, emulsifier, or thickener in cosmetics",
+    moreinfo: null
+  },
+  "Ppg-14 Butyl Ether": {
+    def: "Chemical derived from petruleum processing used to enhance the appearance of dry or damaged skin by reducing flaking and restoring suppleness",
+    moreinfo: null
+  },
+  "Dimethicone": {
+    def: "Silicon-based organic polymer used as a surfactant on the skin",
+    moreinfo: null
+  },
+  "C12-15 Alkyl Benzoate": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Hydrogenated Castor Oil": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Talc": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Fragrance (Parfum)": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Helianthus Annuus (Sunflower) Seed Oil": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Steareth-100": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Vegetable Oil": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Glyceryl Oleate": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Glycerin": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Propylene Glycol": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Tbhq": {
+    def: "Definition",
+    moreinfo: null
+  },
+  "Bht": {
+    def: "Definition",
+    moreinfo: null
+  },
